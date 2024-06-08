1eb7e2a71bf922d9f70d130b9a647be45c1281d0
daohu527 <daohu527@gmail.com>
2024-06-08 22:59:12 +0800

    feat: update version to 7.0.0

--- core/setup.bash
+++ core/setup.bash
@@ -6,7 +6,7 @@ export APOLLO_BAZEL_DIST_DIR="${APOLLO_CACHE_DIR}/distdir"
 export CYBER_PATH="${APOLLO_ROOT_DIR}/cyber"
 
 bazel_bin_path="${APOLLO_ROOT_DIR}/bazel-bin"
-cyber_bin_path="${bazel_bin_path}/cyber"
+mainboard_path="${bazel_bin_path}/cyber/mainboard"
 cyber_tool_path="${bazel_bin_path}/cyber/tools"
 recorder_path="${cyber_tool_path}/cyber_recorder"
 launch_path="${cyber_tool_path}/cyber_launch"
@@ -15,21 +15,18 @@ node_path="${cyber_tool_path}/cyber_node"
 service_path="${cyber_tool_path}/cyber_service"
 monitor_path="${cyber_tool_path}/cyber_monitor"
 visualizer_path="${bazel_bin_path}/modules/tools/visualizer"
-rosbag_to_record_path="${bazel_bin_path}/modules/data/tools/rosbag_to_record"
 
-# TODO(all): place all these in one place and add_to_path
-for entry in "${cyber_bin_path}" \
+# TODO(all): place all these in one place and pathprepend
+for entry in "${mainboard_path}" \
     "${recorder_path}" "${monitor_path}"  \
     "${channel_path}" "${node_path}" \
     "${service_path}" \
     "${launch_path}" \
-    "${visualizer_path}" \
-    "${rosbag_to_record_path}" ; do
-    add_to_path "${entry}"
+    "${visualizer_path}" ; do
+    pathprepend "${entry}"
 done
 
-# ${CYBER_PATH}/python
-export PYTHONPATH=${bazel_bin_path}/cyber/python/internal:${PYTHONPATH}
+pathprepend ${bazel_bin_path}/cyber/python/internal PYTHONPATH
 
 export CYBER_DOMAIN_ID=80
 export CYBER_IP=127.0.0.1
@@ -42,7 +39,6 @@ export GLOG_minloglevel=0
 export sysmo_start=0
 
 # for DEBUG log
-#export GLOG_minloglevel=-1
 #export GLOG_v=4
 
 source ${CYBER_PATH}/tools/cyber_tools_auto_complete.bash
