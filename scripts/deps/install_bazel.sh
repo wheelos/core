6549b8cb6c15240c8304491b9c00d48a468ecc58
daohu527 <daohu527@gmail.com>
2024-06-12 23:39:56 +0800

    fix: fix install bazel

--- scripts/deps/install_bazel.sh
+++ scripts/deps/install_bazel.sh
@@ -24,17 +24,18 @@ CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 TARGET_ARCH=$(uname -m)
 
-BAZEL_VERSION="7.2.0"
+BAZEL_VERSION=$(<"${CURR_DIR}/../../.bazelversion")
+echo "Start installing bazel ${BAZEL_VERSION}"
 
 if [ "$TARGET_ARCH" == "x86_64" ]; then
-  # https://docs.bazel.build/versions/master/install-ubuntu.html
+  # https://bazel.build/install/ubuntu#binary-installer
   PKG_NAME="bazel_${BAZEL_VERSION}-linux-x86_64.deb"
   DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${PKG_NAME}"
   SHA256SUM="70c0166145f649d53f1306f19ff8763ff3ac5777f0e9eb48e30f4abb0dfb4caf"
   download_if_not_cached $PKG_NAME $SHA256SUM $DOWNLOAD_LINK
 
   apt_get_update_and_install \
-    zlib1g-dev
+    g++ unzip zip
 
   # https://docs.bazel.build/versions/master/install-ubuntu.html#step-3-install-a-jdk-optional
   # openjdk-11-jdk
@@ -59,27 +60,26 @@ if [ "$TARGET_ARCH" == "x86_64" ]; then
 
 elif [ "$TARGET_ARCH" == "aarch64" ]; then
   ARM64_BINARY="bazel-${BAZEL_VERSION}-linux-arm64"
-  CHECKSUM="07955cbef922b51025577df4e258d5dfc4f7adc5ec8ab110dedb411878d63627"
+  CHECKSUM="d621ba80c471531e208773f005913169ce226d885eebd9c81ca156bd25761149"
   DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${ARM64_BINARY}"
-  # https://github.com/bazelbuild/bazel/releases/download/3.4.1/bazel-3.4.1-linux-arm64
+  # https://github.com/bazelbuild/bazel/releases/download/7.2.0/bazel-7.2.0-linux-arm64
   download_if_not_cached "${ARM64_BINARY}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
   cp -f ${ARM64_BINARY} "${SYSROOT_DIR}/bin/bazel"
   chmod a+x "${SYSROOT_DIR}/bin/bazel"
   rm -rf "${ARM64_BINARY}"
 
   cp /opt/apollo/rcfiles/bazel_completion.bash /etc/bash_completion.d/bazel
-
-  BUILDTOOLS_VERSION="3.3.0"
-  PKG_NAME="buildifier-${BUILDTOOLS_VERSION}-linux-arm64"
-  CHECKSUM="19d5b358cb099e264086b26091661fd7548df0a2400e47fd98238cfe0a3e67f9"
-  DOWNLOAD_LINK="https://apollo-system.cdn.bcebos.com/archive/6.0/${PKG_NAME}"
+  
+  ## buildifier ##
+  BUILDTOOLS_VERSION="7.1.2"
+  PKG_NAME="buildifier-linux-arm64"
+  CHECKSUM="c22a44eee37b8927167ee6ee67573303f4e31171e7ec3a8ea021a6a660040437"
+  DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/v${BUILDTOOLS_VERSION}/${PKG_NAME}"
   download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
 
   cp -f ${PKG_NAME} "${SYSROOT_DIR}/bin/buildifier"
   chmod a+x "${SYSROOT_DIR}/bin/buildifier"
   rm -rf ${PKG_NAME}
-  # buildozer can be retrieved from
-  # https://apollo-platform-system.bj.bcebos.com/archive/6.0/buildozer-3.3.0-linux-arm64
 
   info "Done installing bazel ${BAZEL_VERSION} with buildifier ${BUILDTOOLS_VERSION}"
 else
@@ -87,10 +87,7 @@ else
   exit 1
 fi
 
-# Note(storypku):
-# Used by `apollo.sh config` to determine native cuda compute capability.
-bash ${CURR_DIR}/install_device_query.sh
-
 # Clean up cache to reduce layer size.
 apt-get clean && \
     rm -rf /var/lib/apt/lists/*
+
